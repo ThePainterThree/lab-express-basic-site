1e31ec40f7599d04daf83de290fbec1d5d9e9089
Patricia <marie.lou.trips@gmail.com>
2023-05-19 23:21:02 +0200

    route add

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 4000
+const path = require('path')
 
-//app.use(express.static('public'));
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get("/", (req, res) => {
-    response.send("Welcome");
+    res.send("Welcome");
   });
-
 
   app.get("/about", (req, res) => {
-    response.send("About");
+    res.send("About");
   });
+
+  app.get("/works", (req, res) => {
+    res.send("Works");
+  });
+
+  app.get("/gallery", (req, res) => {
+    res.send("Gallery");
+  });
+
+  app.listen(port, () => console.log(`App is listening to the port ${port}`));
