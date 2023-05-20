107ff5128a0a4ae96326633b4757c351c4d3f20c
Patricia <marie.lou.trips@gmail.com>
2023-05-20 15:54:49 +0200

    changes on app file

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 4000
+const port = 3001;
 const path = require('path')
 
 app.use('/', express.static(path.join(__dirname, 'public')))
+
 
 app.get("/", (req, res) => {
     res.send("Welcome");
