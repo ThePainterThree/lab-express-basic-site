0185b1447f272c8a1bbce97a8902c6ed6efe1400
Patricia <marie.lou.trips@gmail.com>
2023-05-28 23:34:45 +0200

    Add css

--- app.js
+++ app.js
@@ -3,26 +3,23 @@ const app = express();
 const port = 3000;
 const path = require('path')
 
-app.use('/', express.static(path.join(__dirname, 'public')))
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/views/index.html`)
-  });
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
 
-  app.get("/about", (req, res) => {
-    res.sendFile(`${__dirname}/views/about.html`)
-  });
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
 
-  app.get("/work", (req, res) => {
-      res.sendFile(path.join(__dirname, 'views', 'work.html'));
-    //res.sendFile(`${__dirname}/views/work.html`)
-    
-  });
+app.get("/work", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'work.html'))
+})
 
-  app.get("/gallery", (req, res) => {
-    res.sendFile(`${__dirname}/views/gallery.html`)
-  });
+app.get("/gallery", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'gallery.html'))
+})
 
-  app.listen(port, () => console.log(`App is listening to the port ${port}`));
+app.listen(port, () => console.log(`App is listening to the port ${port}`));
